19979711b0ab37c0b315a3f1c5023ca590242c5c
Daniil Barklon <daniil.barklon@lightspeedhq.com>
2022-03-13 17:26:15 -0400

    make jest stop complaining by detecting open handles

--- announcer/jest.config.js
+++ announcer/jest.config.js
@@ -12,6 +12,8 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
 
+  detectOpenHandles: true,
+
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
   globals: {
