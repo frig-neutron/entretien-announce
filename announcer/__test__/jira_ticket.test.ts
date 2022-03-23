571b7506b21a466f47c8823c22e82b6775fc0326
Daniil Barklon <daniil.barklon@lightspeedhq.com>
2022-03-23 11:38:37 -0400

    ticket: parse building

--- announcer/__test__/jira_ticket.test.ts
+++ announcer/__test__/jira_ticket.test.ts
@@ -5,8 +5,13 @@ import {JiraTicket, proxyJiraJsIssue} from "../src/jira_ticket";
 
 
 describe("jira ticket", () => {
-  const issue: Issue = mockDeep()
-  const ticket: JiraTicket = proxyJiraJsIssue(issue)
+  let issue = mock<Issue>()
+  let ticket: JiraTicket = proxyJiraJsIssue(issue)
+
+  beforeEach(() => {
+    issue = mock<Issue>()
+    ticket = proxyJiraJsIssue(issue)
+  })
 
   test("issue key", () => {
     issue.key = "ISSUE_KEY"
@@ -18,4 +23,24 @@ describe("jira ticket", () => {
     expect(ticket.summary()).toEqual("SUMMARY")
   })
 
+  test("building number from field", () => {
+    issue.fields.customfield_10038 = {
+      value: "1234"
+    }
+    expect(ticket.building()).toEqual("1234")
+  })
+
+  test("building number from summary if field empty", () => {
+    issue.fields.summary = "1234 title"
+    expect(ticket.building()).toEqual("1234")
+  })
+
+  test("building number unknown", () => {
+    issue.fields.summary = "no building title"
+    expect(ticket.building()).toEqual("unknown")
+  })
+
+  test("building number unknown if no title set", () => {
+    expect(ticket.building()).toEqual("unknown")
+  })
 })
--- announcer/src/jira_ticket.ts
+++ announcer/src/jira_ticket.ts
@@ -5,14 +5,42 @@ import {Issue} from "jira.js/out/version2/models";
 
 export interface JiraTicket {
   key(): string
+
   building(): string,
+
   summary(): string
 }
 
 export function proxyJiraJsIssue(issue: Issue): JiraTicket {
+
+  const value = (maybeField: any): string | undefined => {
+    if (maybeField && maybeField.value) {
+      return maybeField.value
+    } else {
+      return undefined
+    }
+  }
+
+  const parseBuilding = (): string => {
+    const buildingFromField = value(issue.fields.customfield_10038);
+    const summary = issue.fields.summary;
+    if (buildingFromField) {
+      return buildingFromField
+    } else {
+      const bldgRe = /^([0-9]+).*/
+      if (summary) {
+        const match = summary.match(bldgRe)
+        if (match) {
+          return match[1]
+        }
+      }
+      return "unknown"
+    }
+  }
+
   return {
     key: () => issue.key,
-    building: () => "",
+    building: () => parseBuilding(),
     summary: () => issue.fields.summary
   }
 }
