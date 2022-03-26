9e5443d7a9fa41eccc92c084895ef93717b7b15e
Daniil Barklon <frig.neutron@gmail.com>
2022-03-26 12:15:20 -0400

    launcher script

--- announcer/scripts/deploy.sh
+++ announcer/scripts/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash 
-gcloud functions deploy announcer --project=entretien-prd --max-instances=1 --runtime=nodejs16 --trigger-topic=announcer_trigger
+project_id=entretien-prd
+gcloud functions deploy announcer --project=$project_id --max-instances=1 \
+  --runtime=nodejs16 --trigger-topic=announcer_trigger \
+  --service-account=announcer@entretien-prd.iam.gserviceaccount.com \
+  --set-secrets=ANNOUNCER_SECRETS=announcer:1
