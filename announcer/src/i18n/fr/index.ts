7e49833c08c1481e3337f6f2b06cd3e5bf2d3615
Daniil Barklon <daniil.barklon@lightspeedhq.com>
2022-04-23 09:53:00 -0400

    input french translation

--- announcer/src/i18n/fr/index.ts
+++ announcer/src/i18n/fr/index.ts
@@ -1,7 +1,38 @@
-import type { BaseTranslation } from '../i18n-types'
+import type {BaseTranslation} from '../i18n-types'
 
 const fr: BaseTranslation = {
-	subject: 'Rapport de billetterie pour {interval:Interval|subjectReportInterval}'
+  subject: 'Rapport de ticket pour {interval:Interval|subjectReportInterval}',
+  greeting: 'Cher {name:string},',
+  preamble: `Voici un résumé de l'activité des tickets jira pour {month:DateTime|monthYear}`,
+  issue: {
+    age: {
+      header: 'L\'âge du ticket',
+      value: '{duration:Duration|durationInDays}'
+    },
+    key: {
+      header: 'Numéro de ticket'
+    },
+    status: {
+      header: 'Statut du ticket'
+    },
+    summary: {
+      header: 'Sommaire',
+    }
+  },
+  missingValue: "Inconnu",
+  outtro: [
+    'Cet e-mail a été envoyé par un système automatisé. ',
+    'Si vous ne souhaitez pas recevoir ces messages, veuillez contacter Daniil'
+  ].join(''),
+  created: {
+    heading: "Tickets créés entre {start:DateTime|dtHeader} et {end:DateTime|dtHeader}"
+  },
+  allTickets: {
+    heading: "Tous les tickets en cours"
+  },
+  closed: {
+    heading: "Tickets terminés entre {start:DateTime|dtHeader} et {end:DateTime|dtHeader}"
+  },
 }
 
 export default fr
