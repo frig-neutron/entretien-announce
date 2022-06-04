2dc536a5d80b2a0abef868dfec4c4cf97cea3979
Daniil Barklon <frig.neutron@gmail.com>
2022-06-04 17:51:37 -0400

    add publish config to function

--- announcer/scripts/deploy.sh
+++ announcer/scripts/deploy.sh
@@ -22,4 +22,5 @@ gcloud functions deploy announcer --project=$project_id --max-instances=1 \
   --runtime=nodejs16 --trigger-topic=announcer_trigger \
   --service-account=announcer@$project_id.iam.gserviceaccount.com \
   --set-secrets=ANNOUNCER_SECRETS=announcer:latest \
+  --update-env-vars=ANNOUNCEMENT_PUBLISH_CONFIG='{"project_id": "'$project_id'", "topic_name": "sendmail"}' \
   --update-env-vars=^--^DIRECTORY="`cat $dirfile`" 
