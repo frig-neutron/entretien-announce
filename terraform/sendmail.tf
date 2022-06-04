a74baeef0ae22d4f1fdf93bf3ca9403b553a4f59
Daniil Barklon <frig.neutron@gmail.com>
2022-06-04 18:04:38 -0400

    tf: create backup sub for email

--- terraform/sendmail.tf
+++ terraform/sendmail.tf
@@ -3,3 +3,13 @@ resource "google_pubsub_topic" "sendmail" {
   name    = "sendmail"
   labels  = {}
 }
+
+resource "google_pubsub_subscription" "sendmail_backup" {
+  project = google_project.entretien.project_id
+  name    = "sendmail-backup"
+  topic   = google_pubsub_topic.sendmail.name
+
+  expiration_policy {
+    ttl = 0
+  }
+}
