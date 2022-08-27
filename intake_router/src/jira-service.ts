0574625ede131c664e6c1311524316a1c25e7e5b
Daniil Barklon <daniil.barklon@lightspeedhq.com>
2022-08-27 13:53:43 -0400

    todo

--- intake_router/src/jira-service.ts
+++ intake_router/src/jira-service.ts
@@ -3,6 +3,8 @@ import {IntakeFormData} from "./intake-form-data";
 export function jiraService(): JiraService {
   return {
     createIssue(intakeFormData: IntakeFormData): Promise<String> {
+      // TODO: idempotence - replaying the same issue multiple times should not re-create issue
+      // probably a good idea to use a hidden field w/ form data hash
       return Promise.resolve("");
     }
 
