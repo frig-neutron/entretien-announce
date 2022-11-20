0b8ddbe89d9e6deba04204379828aad7595f8e76
Daniil Barklon <daniil.barklon@lightspeedhq.com>
2022-11-20 16:32:59 -0500

    at least one email matches matcher

--- intake_router/test/ticket-announcer.test.ts
+++ intake_router/test/ticket-announcer.test.ts
@@ -3,34 +3,6 @@ import {ticketAnnouncer} from "../src/ticket-announcer";
 import {Announcement} from "struct_lalliance/build/src/announcement";
 import CustomMatcherResult = jest.CustomMatcherResult;
 
-expect.extend({
-  emailMatches(received: Announcement, expectedEmail: EmailSpec): CustomMatcherResult {
-
-    expect(received.primary_recipient).toBe(expectedEmail.to.email)
-    expect(received.subject).toBe(expectedEmail.subject)
-
-    const bodyRe = (s: string) => expect(received.body).toMatch(new RegExp(s))
-
-    bodyRe(expectedEmail.source.reporter + " has submitted " +
-        (expectedEmail.isUrgent ? "an URGENT" : "a") +
-        " maintenance report")
-
-    const jiraSummary = ((f: IntakeFormData) => f.building + " " + f.area + ": " + f.summary)(expectedEmail.source);
-
-    bodyRe("^Dear " + expectedEmail.to.name)
-    bodyRe( "\n" + jiraSummary + "\n" + expectedEmail.source.description)
-    bodyRe("\nYou are receiving this email because " + expectedEmail.reasonForReceiving)
-    bodyRe(
-        "\nJira ticket https://lalliance.atlassian.net/browse/" + expectedEmail.issueKey +
-        " has been assigned to this report."
-    )
-
-    return {
-      pass: true, message: () => "ummm 🙄"
-    }
-  }
-})
-
 describe("ticket announcer", () => {
   const issueKey = "PROJ-" + Math.floor(Math.random() * 1000)
   describe("non-urgent", () => {
@@ -45,12 +17,11 @@ describe("ticket announcer", () => {
       summary: "chauffe-eau"
     }
 
-    test("end-to-end, non-urgent", () => {
-
+    test("route to BR", () => {
       const announcer = ticketAnnouncer();
       const announcements = announcer.emailAnnouncement(issueKey, formValues);
 
-      expect(announcements[0]).emailMatches({
+      expect(announcements).someEmailMatches({
         to: {
           email: "br-3737@email.com",
           name: "BR for 3737"
@@ -67,6 +38,8 @@ describe("ticket announcer", () => {
 
 interface EmailMatchers {
   emailMatches(emailSpec: EmailSpec): CustomMatcherResult
+
+  someEmailMatches(emailSpec: EmailSpec): CustomMatcherResult
 }
 
 declare global {
@@ -92,3 +65,68 @@ export type EmailSpec = {
   isUrgent: boolean,
   issueKey: string
 }
+
+
+expect.extend({
+  someEmailMatches(received: Announcement[], expectedEmail: EmailSpec): CustomMatcherResult {
+    type ErrOrMatchResult = CustomMatcherResult | Error
+
+    const requireError = (e: unknown): Error => {
+      if (e instanceof Error)
+        return e
+      else
+        throw Error(`${e} should be of type Error, but it was something else`)
+    }
+
+    const Catch = (f: (() => CustomMatcherResult)): ErrOrMatchResult => {
+      try {
+        return f()
+      } catch (assertionError: unknown) {
+        return requireError(assertionError);
+      }
+    }
+
+    const assertionErrorOrUndefined: ErrOrMatchResult[] = received.map(actualEmail =>
+        Catch(() => expect(actualEmail).emailMatches(expectedEmail))
+    )
+
+    const isSuccess = (e: any): boolean => typeof e == "undefined" // no error == success
+    const isFailure = (e: any): boolean => !isSuccess(e)
+    const atLeastOneMatch = assertionErrorOrUndefined.map(isSuccess).filter((i: boolean) => i).length > 0
+    const getMessage = (e: ErrOrMatchResult) => e instanceof Error ? e.message : e.message()
+
+    const failures = assertionErrorOrUndefined.filter(isFailure);
+    return {
+      pass: atLeastOneMatch,
+      message: () => {
+        const matchFailures: string[] = failures.map(getMessage)
+        return `No email matches spec ${JSON.stringify(expectedEmail, null, 2)}\n` + matchFailures.join("\n")
+      }
+    }
+  },
+  emailMatches(received: Announcement, expectedEmail: EmailSpec): CustomMatcherResult {
+
+    expect(received.primary_recipient).toBe(expectedEmail.to.email)
+    expect(received.subject).toBe(expectedEmail.subject)
+
+    const bodyRe = (s: string) => expect(received.body).toMatch(new RegExp(s))
+
+    bodyRe(expectedEmail.source.reporter + " has submitted " +
+        (expectedEmail.isUrgent ? "an URGENT" : "a") +
+        " maintenance report")
+
+    const jiraSummary = ((f: IntakeFormData) => f.building + " " + f.area + ": " + f.summary)(expectedEmail.source);
+
+    bodyRe("^Dear " + expectedEmail.to.name)
+    bodyRe("\n" + jiraSummary + "\n" + expectedEmail.source.description)
+    bodyRe("\nYou are receiving this email because " + expectedEmail.reasonForReceiving)
+    bodyRe(
+        "\nJira ticket https://lalliance.atlassian.net/browse/" + expectedEmail.issueKey +
+        " has been assigned to this report."
+    )
+
+    return {
+      pass: true, message: () => "ummm 🙄"
+    }
+  }
+})
