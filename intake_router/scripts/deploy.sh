6307584f025ed2112d5a49e8efc23524e681edda
Daniil Barklon <frig.neutron@gmail.com>
2023-04-22 15:11:05 -0400

    deploy script

--- intake_router/scripts/deploy.sh
+++ intake_router/scripts/deploy.sh
@@ -7,16 +7,7 @@ fi
 
 set -euo pipefail
 
-cwd=`dirname $0`
 project_id=entretien-$env
-dirfile="$cwd/directory.$project_id.json"
-
-if [ ! -r $dirfile ]; then 
-  echo "$dirfile is not readable for env=$env"
-  echo "    known directory files: "
-  find $cwd -name 'directory.*.json' -exec basename {} \; | sed -e 's/^/    - /'
-  exit 1
-fi 
 
 gcloud functions deploy intake_router --project=$project_id --max-instances=2 \
   --runtime=nodejs16 --trigger-http --allow-unauthenticated \
