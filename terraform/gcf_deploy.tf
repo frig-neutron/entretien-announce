9c212c781123ff88f39f36e639308562c325747e
Daniil Barklon <frig.neutron@gmail.com>
2023-04-30 14:26:23 -0400

    function sources bucket

--- terraform/gcf_deploy.tf
+++ terraform/gcf_deploy.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "gcf_sources" {
   project                     = google_project.entretien.project_id
   name                        = "gcf_sources_${terraform.workspace}"
-  location                    = "us" uniform_bucket_level_access = true
+  location                    = "us" 
+  uniform_bucket_level_access = true
 }
