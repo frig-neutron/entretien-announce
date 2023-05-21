46bdef925906d63a37eb916a0f9ed21b92683561
Daniil Barklon <daniil.barklon@lightspeedhq.com>
2023-05-20 21:00:59 -0400

    intake_router: use function source dir

--- intake_router/scripts/deploy.sh
+++ intake_router/scripts/deploy.sh
@@ -1,17 +1,22 @@
-#!/bin/bash 
+#!/bin/bash
 
-if [ -z "$env" ]; then 
+if [ -z "$env" ]; then
   echo "usage: env={stg,prd} $0"
   exit 1
 fi
 
 set -euo pipefail
 
+. "`dirname $0`/../scripts/_include.sh"
+
+require_function_root
+
 project_id=entretien-$env
 cwd=`dirname $0`
 
 gcloud functions deploy intake_router --project=$project_id --max-instances=2 \
+  --source="`gcf_source_path $project_id`" \
   --runtime=nodejs16 --trigger-http --allow-unauthenticated \
   --env-vars-file="$cwd/env-$env.yaml" \
   --set-secrets=SECRETS=announcer:latest \
-  --service-account=intake-router@$project_id.iam.gserviceaccount.com 
+  --service-account=intake-router@$project_id.iam.gserviceaccount.com
