584bcc3043c6a484c24fdb16bd5ede2ad50a8098
Dan Barklon <frig.neutron@gmail.com>
2025-09-02 00:05:23 +0000

    bump nodejs runtime

--- intake_router/scripts/deploy.sh
+++ intake_router/scripts/deploy.sh
@@ -24,7 +24,7 @@ validate_config_key JIRA_OPTIONS $vars_file
 
 gcloud functions deploy intake_router --project=$project_id --max-instances=2 \
   --source="$source_path" \
-  --runtime=nodejs16 --trigger-http --allow-unauthenticated \
+  --runtime=nodejs20 --trigger-http --allow-unauthenticated \
   --env-vars-file="$vars_file" \
   --set-secrets=SECRETS=announcer:latest \
   --service-account=intake-router@$project_id.iam.gserviceaccount.com
